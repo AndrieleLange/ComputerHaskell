94217c9269f0985ab683410beee73639629dfbaf
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2024-06-27 16:04:14 -0300

    limpando e pensando

--- Computer.hs
+++ Computer.hs
@@ -1,29 +1,23 @@
--- O programa começa no 0
+import System.IO 
+
 type ListMemoria = [(Int, Int)] -- lista de memorias
 
-data CPU = CPU { acc :: Int, pc :: Int, memoria :: ListMemoria } deriving (Show)
+data CPU = CPU { acc :: Int, pc :: Int, memoria :: ListMemoria, eqz :: Int } deriving (Show)
 
 -- Cria CPU
 criaCPU :: Int -> Int -> ListMemoria -> CPU
 criaCPU acc pc memoria = CPU { acc = acc, pc = pc, memoria = memoria, eqz = eqz }
 
-
 -- Escrever na memória
--- Armazenar o conteúdo em um endereço de memória
--- writeMem(memoria,endereço,conteudo)=memoria
--- Exemplo:
--- wirteMem [(0,10),(1,3),(2,23),(10,100)] 1 6 = [(0,10),(1,6),(2,23),(10,100)]
--- Salvar na memoria
 salvaMemoria :: ListMemoria -> Int -> Int -> ListMemoria
-salvaMemoria memoria  =  (e, c) : ListMemoria
-
+salvaMemoria [] endereco conteudo = [(endereco, conteudo)]
+salvaMemoria ((e, c):xs) endereco conteudo
+    | e == endereco = (e, conteudo) : xs
+    | otherwise     = (e, c) : salvaMemoria xs endereco conteudo
 
 -- Ler a memória
--- Retornar o conteúdo do endereço de memória
--- readMem(memoria,endereco)=conteudo
--- Exemplo:
--- readMem [(0,10),(1,3),(2,23),(10,100)] 1 = 3
 readMem :: ListMemoria -> Int -> Int
+readMem [] _ = 0
 readMem (x:xs) e
     | e == fst x = snd x
     | e /= fst x = readMem xs e
@@ -31,18 +25,52 @@ readMem (x:xs) e
 
 -- Atualizar conteúdo na memória
 atualizaMemoria :: Int -> Int -> ListMemoria -> ListMemoria
-atualizaMemoria _ _ [] = salvaMemoria endereco cont ((e, c):xs)  -- Se a memória estiver vazia, salva
-atualizaMemoria endereco cont ((e, c):xs)
-    | endereco == e     = (e, cont) : xs  -- Atualiza o valor se o endereço for encontrado
-    | otherwise         = atualizaMemoria endereco cont xs  -- Continua procurando
+atualizaMemoria endereco conteudo memoria = salvaMemoria memoria endereco conteudo
 
-    
 
--- Tanto os códigos de instrução como o conteúdo dos endereços de memória 
--- poderão ser informados em 
--- decimal, desde que respeitados o tamanho máximo de bits correspondentes 
--- ao valor binário associado.
+-- instrução LOD
+-- execLOD (endereço, memória, acc, eqz) -> (endereço, memória, acc, eqz)
+execLOD :: Int -> CPU -> CPU
+execLOD end cpu = cpu {acc = readMem (memoria cpu) end}
 
+
+-- instrução STO
+-- execSTO end (ListMemoria, acc, eqz) -> (ListMemoria, acc, eqz)
+execSTO :: Int -> CPU -> CPU
+execSTO end cpu = cpu {memoria = atualizaMemoria end (acc mem)}
+
+-- instrução CPE
+-- execCPE (endereço, memória, acc, eqz) -> (endereço, memória, acc, eqz)
+execCPE :: Int -> CPU -> CPU
+execCPE end cpu = cpu {
+    eqz = if readMem memoria end == acc
+    then 0
+    else 1
+}
+
+-- Instrução ADD
+execADD :: Int -> CPU -> CPU
+execADD end cpu = cpu{ acc = newAcc}
+    where 
+        val = readMem mem end
+        newAcc = val + acc
+
+-- instrução SUB
+execSUB :: Int -> CPU -> CPU
+execSUB end cpu = cpu {acc = newAcc}
+    where 
+        val = readMem mem end
+        newAcc = acc - val
+
+
+-- instrução NOP
+-- execNOP(mem,acc,eqz) = (mem,acc,eqz)
+execNOP :: CPU -> CPU 
+execNOP cpu = cpu
+
+-- instrução HLT
+execHLT :: CPU -> CPU 
+execHLT cpu = cpu {pc = -1}
 
 
 -- Executar um programa
@@ -55,54 +83,15 @@ executar mem =
 -- faz até encontrar o NOP
 -- criar uma função de cada instrução
 
--- instrução LOD
--- execLOD (endereço, memória, acc, eqz) -> (endereço, memória, acc, eqz)
-execLOD :: Int -> (ListMemoria, Int, Int) -> (ListMemoria, Int, Int)
-execLOD end (mem,acc,eqz) = (mem,readMem mem end,eqz)
 
--- instrução STO
--- execSTO end (ListMemoria, acc, eqz) -> (ListMemoria, acc, eqz)
-execSTO :: Int -> (ListMememoria, Int, Int) -> (ListMemoria, Int, Int)
-execSTO end (mem, acc, eqz) = (atualizaMemoria end acc mem, acc, eqz)
 
 -- Instrução JMP
-execJMP Int -> (ListMememoria, Int, Int) -> (ListMemoria, Int, Int)
-execJMP end (mem, acc, eqz) = 
-    pc = readMem end mem 
-
-
-
+execJMP Int -> CPU -> CPU
+execJMP end cpu = cpu { pc = readMem mem end}
 
 -- instrução JMZ
-
--- Instrução ADD
-execADD :: Int -> (ListMememoria, Int, Int) -> (ListMemoria, Int, Int)
-execADD end (mem,acc,eqz) = (mem, newAcc, eqz)
-    where 
-        val = readMem mem end
-        newAcc = val + acc
-
--- instrução SUB
-execSUB :: Int -> (ListMememoria, Int, Int) -> (ListMemoria, Int, Int)
-execSUB end (mem,acc,eqz) = (mem, newAcc, eqz)
-    where 
-        val = readMem mem end
-        newAcc = val - acc
-
-
--- Instrução HLT
-
--- instrução CPE
--- execCPE (endereço, memória, acc, eqz) -> (endereço, memória, acc, eqz)
-execCPE :: Int -> (ListMemoria, Int, Int) -> (ListMemoria, Int, Int)
-execCPE end (mem, acc, eqz) =
-    if readMem mem end == acc
-    then (mem, 0, eqz)
-    else (mem, 1, eqz)
-
-
--- instrução NOP
--- execNOP(mem,acc,eqz) = (mem,acc,eqz)
-execNOP :: (ListMemoria, Int, Int) -> (ListMemoria, Int, Int) 
-execNOP (mem,acc,eqz) = id -- provavelmente incompleto
-
+execJMZ Int -> CPU -> CPU
+execJMZ end cpu = 
+    if acc cpu == 0
+        then execJMP end cpu
+        else cpu
