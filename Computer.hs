923e42729d44e3f95cef4065576b3d8bd05247f7
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2024-06-28 17:21:08 +0000

    acho que n ta funcionando

--- Computer.hs
+++ Computer.hs
@@ -18,9 +18,9 @@ salvaMemoria ((e, c):xs) endereco conteudo
 -- Ler a memória
 readMem :: ListMemoria -> Int -> Int
 readMem [] _ = 0
-readMem (x:xs) e
-    | e == fst x = snd x
-    | e /= fst x = readMem xs e
+readMem ((e, c):xs) endereco
+    | e == endereco = c
+    | otherwise     = readMem xs endereco
 
 
 -- Atualizar conteúdo na memória
@@ -37,7 +37,7 @@ execLOD end cpu = cpu {acc = readMem (memoria cpu) end}
 -- instrução STO
 -- execSTO end (ListMemoria, acc, eqz) -> (ListMemoria, acc, eqz)
 execSTO :: Int -> CPU -> CPU
-execSTO end cpu = cpu {memoria = atualizaMemoria end (acc mem)}
+execSTO end cpu = cpu {memoria = atualizaMemoria end (acc cpu) (acc mem)}
 
 -- instrução CPE
 -- execCPE (endereço, memória, acc, eqz) -> (endereço, memória, acc, eqz)
@@ -95,3 +95,41 @@ execJMZ end cpu =
     if acc cpu == 0
         then execJMP end cpu
         else cpu
+
+
+
+-- Executar um programa
+executar :: CPU -> CPU
+executar cpu
+    | pc cpu < 0 = cpu -- Se pc for -1, o programa parou
+    | otherwise =
+        let instr = readMem (memoria cpu) (pc cpu)
+            end = readMem (memoria cpu) (pc cpu + 1)
+            newCPU = case instr of
+                2  -> execLOD end cpu
+                4  -> execSTO end cpu
+                6  -> execJMP end cpu
+                8  -> execJMZ end cpu
+                10 -> execCPE end cpu
+                14 -> execADD end cpu
+                16 -> execSUB end cpu
+                18 -> execNOP cpu
+                20 -> execHLT cpu
+                _  -> cpu -- Instrução desconhecida, não faz nada
+        in traceCPU newCPU { pc = pc newCPU + 2 }
+  where
+    traceCPU cpu = cpu { memoria = trace (showCPU cpu) (memoria cpu) }
+    showCPU cpu = "CPU { acc: " ++ show (acc cpu) ++ ", pc: " ++ show (pc cpu) ++ ", eqz: " ++ show (eqz cpu) ++ " }"
+
+-- Leitura da memória de um arquivo
+carregaMemoria :: FilePath -> IO ListMemoria
+carregaMemoria caminho = do
+    conteudo <- readFile caminho
+    return (map (\linha -> let [end, val] = map read (words linha) in (end, val)) (lines conteudo))
+
+main :: IO ()
+main = do
+    memoria <- carregaMemoria "sub.txt"
+    let cpu = criaCPU 0 0 memoria
+    let cpuFinal = executar cpu
+    print cpuFinal
